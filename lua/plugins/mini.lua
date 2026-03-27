d046f20da3a888f8fee9bc9f76e356cc263cba4a
fatRat <cscode117@gmail.com>
2026-03-27 17:13:31 +0800

    delete autopair plugin, and add the mini.autopairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -30,6 +30,50 @@ return {
 				use_icons = vim.g.have_nerd_font,
 			})
 
+			local autopairs = require("mini.pairs")
+			autopairs.setup( -- No need to copy this inside `setup()`. Will be used automatically.
+				{
+					-- In which modes mappings from this `config` should be created
+					modes = { insert = true, command = false, terminal = false },
+
+					-- Global mappings. Each right hand side should be a pair information, a
+					-- table with at least these fields (see more in |MiniPairs.map|):
+					-- - <action> - one of 'open', 'close', 'closeopen'.
+					-- - <pair> - two character string for pair to be used.
+					-- By default pair is not inserted after `\`, quotes are not recognized by
+					-- <CR>, `'` does not insert the pair after a letter.
+					-- Only parts of tables can be tweaked (others will use these defaults).
+					mappings = {
+						["("] = { action = "open", pair = "()", neigh_pattern = "^[^\\]" },
+						["["] = { action = "open", pair = "[]", neigh_pattern = "^[^\\]" },
+						["{"] = { action = "open", pair = "{}", neigh_pattern = "^[^\\]" },
+
+						[")"] = { action = "close", pair = "()", neigh_pattern = "^[^\\]" },
+						["]"] = { action = "close", pair = "[]", neigh_pattern = "^[^\\]" },
+						["}"] = { action = "close", pair = "{}", neigh_pattern = "^[^\\]" },
+
+						['"'] = {
+							action = "closeopen",
+							pair = '""',
+							neigh_pattern = "^[^\\]",
+							register = { cr = false },
+						},
+						["'"] = {
+							action = "closeopen",
+							pair = "''",
+							neigh_pattern = "^[^%a\\]",
+							register = { cr = false },
+						},
+						["`"] = {
+							action = "closeopen",
+							pair = "``",
+							neigh_pattern = "^[^\\]",
+							register = { cr = false },
+						},
+					},
+				}
+			)
+
 			-- You can configure sections in the statusline by overriding their
 			-- default behavior. For example, here we set the section for
 			-- cursor location to LINE:COLUMN
